2301c030d6f8af2f385f6a11abf62a05366768fa
Alex Pogue <Pogue.Alexander@mayo.edu>
2024-06-17 19:19:16 -1000

    Turn off strict_slashes in python to allow endpoints without slashes

--- checkoff_python/checkoff_python/__init__.py
+++ checkoff_python/checkoff_python/__init__.py
@@ -6,6 +6,10 @@ def create_app(config_file=None):
 
     app.config.from_pyfile(config_file)
 
+    # Turn off strict_slashes to treat `/todo-item` as valid endpoint. Without this setting, it would 304 redirect to `/todo-item/`.
+    # Default Rust Axum doesn't include the ending slash, and there is no hard-and-fast rule for REST APIs
+    app.url_map.strict_slashes = False
+
     models.init_app(app)
     routes.init_app(app)
     return app
