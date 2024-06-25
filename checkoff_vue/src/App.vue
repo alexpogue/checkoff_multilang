f287d461a2bfa4124fc83cc636b20ac8b7c0ed2d
Alex Pogue <Pogue.Alexander@mayo.edu>
2024-06-25 00:02:18 -1000

    Add simple vue app

---
+++ checkoff_vue/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <HelloWorld :todo-items="todoItems"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      todoItems: null
+    }
+  },
+  mounted() {
+    axios
+      .get('http://localhost:3001/todo-item')
+      .then(response => {
+        this.todoItems = response.data.data;
+        console.log("todoItems = " + JSON.stringify(this.todoItems));
+      })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ checkoff_vue/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="todo_list">
+    <ul>
+      <li
+        v-for="item in todoItems"
+        v-bind:key="item.id"
+      >
+      {{ item.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    todoItems: Array
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
