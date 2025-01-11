80a4e6abb9bf415b414c8bb57dec0e3ac74439dd
Alex Pogue <Pogue.Alexander@mayo.edu>
2025-01-10 18:11:46 -1000

    Simplify config commented lines

--- checkoff_python/checkoff_python/config.py
+++ checkoff_python/checkoff_python/config.py
@@ -1,5 +1,4 @@
 SECRET_KEY = 'sUp3rS3cr3ttt'
-SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://root:strong_password@127.0.0.1:3307/checkoff_py'
-#SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://root:strong_password@host.docker.internal:3307/checkoff_py'
-#SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://root:strong_password@172.20.176.1:3307/checkoff_py'
+#SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://root:strong_password@127.0.0.1:3307/checkoff_py'
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:strong_password@checkoff-mysql:3306/checkoff_py"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
